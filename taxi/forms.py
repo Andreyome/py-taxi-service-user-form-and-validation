88f1b428d6010b5463fa2569ae718927d4542d54
Andrii Omelchenko <andreyomem@gmail.com>
2025-06-25 19:19:54 +0300

    implement driver create/update/delete

---
+++ taxi/forms.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+from taxi.models import Driver, Car
+
+
+class DriverCreateForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = UserCreationForm.Meta.fields + ("email", "license_number",)
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise forms.ValidationError("Incorrect licence length")
+        if (not license_number[:3].isalpha()
+                or not license_number[3:].isupper()):
+            raise forms.ValidationError("Invalid license number")
+        if not license_number[3:].isdigit():
+            raise forms.ValidationError("Invalid license number")
+        return license_number
+
+
+class DriverLicenseUpdateForm(forms.ModelForm):
+    class Meta(UserCreationForm.Meta):
+        model = get_user_model()
+        fields = ("license_number",)
+
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+        if len(license_number) != 8:
+            raise forms.ValidationError("Incorrect licence length")
+        if not license_number[:3].isalpha() or license_number[3:].islower():
+            raise forms.ValidationError("Invalid license number")
+        if not license_number[3:].isdigit():
+            raise forms.ValidationError("Invalid license number")
+        return license_number
+
+
+class CarForm(forms.ModelForm):
+    drivers = forms.ModelMultipleChoiceField(
+        queryset=Driver.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+    )
+
+    class Meta:
+        model = Car
+        fields = "__all__"
--- taxi/urls.py
+++ taxi/urls.py
@@ -13,6 +13,11 @@ from .views import (
     ManufacturerCreateView,
     ManufacturerUpdateView,
     ManufacturerDeleteView,
+    DriverCreateView,
+    DriverLicenseUpdateView,
+    DriverDeleteView,
+    add_driver,
+    delete_driver
 )
 
 urlpatterns = [
@@ -42,10 +47,19 @@ urlpatterns = [
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
+    path("cars/<int:pk>/add_driver/", add_driver, name="add-driver"),
+    path("cars/<int:pk>/remove_driver/", delete_driver, name="remove-driver"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
     ),
+    path("drivers/create", DriverCreateView.as_view(), name="driver-create"),
+    path("drivers/<int:pk>/update/",
+         DriverLicenseUpdateView.as_view(),
+         name="driver-update"),
+    path("drivers/<int:pk>/delete/",
+         DriverDeleteView.as_view(),
+         name="driver-delete"),
 ]
 
 app_name = "taxi"
